d4d6b0560740005e3b88ef94b6fa5d8f7a48fe1d
Mike <misha-telepov@mail.ru>
2020-11-24 19:22:28 +0400

    Lab 3-2

--- Project1/TList1Reliz.h
+++ Project1/TList1Reliz.h
@@ -3,31 +3,6 @@
 #include <cstdlib>
 #include <iostream>
 
-/*template<typename T> class TList1Elem
-{
-private:
-	T data;
-	TList1Elem* next;
-
-	TList1Elem() { this->next = this; }
-
-	TList1Elem(T newEl) {
-		this->data = newEl;
-	}
-
-	TList1Elem(T newEl, TList1Elem* nextEl) {
-		this->data = newEl;
-		this->next = nextEl;
-	}
-
-	TList1Elem(const TList1Elem& T) {
-		this->data = T.data;
-		this->next = T.next;
-	}
-	friend class TList1Reliz<T>;
-};*/
-
-
 template<typename T>
 class TList1Reliz {
 	TList1Elem<T>* head;
